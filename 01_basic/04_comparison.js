293170786f7337c7a07b64a5a501882974f4afa6
Anuja172 <anujasalunkhe7@gmail.com>
2026-03-03 16:36:03 +0000

    compare

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,9 @@
+console.log("2">1);  //automatic convert string into no
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+//=== strict check
+//example
+console.log("2"==2);   //true
+console.log("2"===2);   //false due to 1 is string and other is no
