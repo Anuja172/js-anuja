acc71e6b927eff88baae03e1ab953f7e689e4919
Anuja172 <anujasalunkhe7@gmail.com>
2026-03-03 13:50:47 +0000

    02_Datatypes

---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,10 @@
+let name="anuja"
+let age= 21
+let isloggedin=true;
+
+//primitive 
+//number,bigint,string,boolean,null=standalone value(ek zero ke barabar value hai),undefined means value nahi hai
+//symbol(uniqueness)
+
+//null ka type object
+//undefined ka type undefined
