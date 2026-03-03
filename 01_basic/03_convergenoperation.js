4d1971b02311c9237bc6fd37ee534722ea86db12
Anuja172 <anujasalunkhe7@gmail.com>
2026-03-03 14:16:46 +0000

    conversion

---
+++ 01_basic/03_convergenoperation.js
@@ -0,0 +1,17 @@
+let score="37abc"
+let score1=null
+console.log(typeof score);
+let valueinnumber = Number(score)  //typeconversion
+let valueinnumber1 = Number(score1) 
+console.log(typeof valueinnumber);
+console.log(valueinnumber);  //nan
+console.log(valueinnumber1);
+
+//"33"=33
+//"33abc"=NaN
+// true=1; false=0
+//null=0
+//undefined=NaN
+
+
+
