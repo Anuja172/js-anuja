277e6ddea04c2fe2d960e4b53debeeed9439b51a
Anuja172 <anujasalunkhe7@gmail.com>
2026-03-03 13:24:57 +0000

    variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,26 @@
+const id=97
+let name="anuja"
+var pass="177"
+city="karad"
+let state
+
+// id=100
+name = "durva"
+pass = "172"
+city="pune"
+
+console.log(id);
+console.log(name);
+console.log(pass);
+console.log(city);
+console.log(state);
+
+/*we did not use var in js beacuse of scope,scope is not fix 
+all var will changes not that particular there4 use let not to use var bcoz 
+block and functional scope*/
+
+
+
+
+
+
