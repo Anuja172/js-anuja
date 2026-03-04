d33885abc4aa9cf955d3ae9f97004d817393d526
Anuja172 <anujasalunkhe7@gmail.com>
2026-03-04 18:35:49 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,5 @@
+//refer all the string topic as it is from java all similar type of methods use in it
+const name="anuja"
+const prn=97
+
+console.log(`hello my name is ${name} and my prn is ${prn}`);
